bab5570132ddfcf6a14812497e2ddddc502ec956
Luket637 <lucasrabello58@gmail.com>
2026-09-23 16:11:30 -0300

    Add PluginInfo class with metadata constants

---
+++ PluginInfo.cs
@@ -0,0 +1,9 @@
+namespace LukisChaosMenu
+{
+    public static class PluginInfo
+    {
+        public const string GUID = "lukis.chaosmenu";
+        public const string Name = "Lukis Chaos Menu";
+        public const string Version = "1.0.0";
+    }
+}
