f4c5b9d601dde03ced4eb6b3ce5765d1441d4e75
Luket637 <lucasrabello58@gmail.com>
2026-09-23 16:13:30 -0300

    Add title object to menu creation
    Added a title object to the menu with specified properties.

--- Menu.cs
+++ Menu.cs
@@ -9,13 +9,14 @@ namespace LukisChaosMenu
         private bool lastYState;
 
         private GameObject menuPanel;
+        private GameObject titleObject;
 
         public void Initialize()
         {
-            CreateMenu();
-
             leftController =
                 InputDevices.GetDeviceAtXRNode(XRNode.LeftHand);
+
+            CreateMenu();
 
             menuPanel.SetActive(false);
         }
@@ -43,6 +44,7 @@ namespace LukisChaosMenu
 
         private void CreateMenu()
         {
+            // Flat 3D panel
             menuPanel = GameObject.CreatePrimitive(
                 PrimitiveType.Quad
             );
@@ -59,11 +61,57 @@ namespace LukisChaosMenu
 
             menuPanel.transform.localScale =
                 new Vector3(0.5f, 0.3f, 1f);
+
+            // Blue panel
+            Renderer panelRenderer =
+                menuPanel.GetComponent<Renderer>();
+
+            panelRenderer.material.color =
+                Color.blue;
+
+            // Black title
+            CreateTitle();
+        }
+
+        private void CreateTitle()
+        {
+            titleObject =
+                new GameObject("Title");
+
+            titleObject.transform.SetParent(
+                menuPanel.transform
+            );
+
+            titleObject.transform.localPosition =
+                new Vector3(0f, 0.05f, -0.01f);
+
+            titleObject.transform.localRotation =
+                Quaternion.identity;
+
+            TextMesh text =
+                titleObject.AddComponent<TextMesh>();
+
+            text.text =
+                "Lukis Chaos Menu";
+
+            text.fontSize = 48;
+            text.characterSize = 0.01f;
+
+            text.anchor =
+                TextAnchor.MiddleCenter;
+
+            text.alignment =
+                TextAlignment.Center;
+
+            text.color =
+                Color.black;
         }
 
         private void ToggleMenu()
         {
-            menuPanel.SetActive(!menuPanel.activeSelf);
+            menuPanel.SetActive(
+                !menuPanel.activeSelf
+            );
         }
     }
 }
